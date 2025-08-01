c26dd704a64479a957f4097238ec393d966be336
Afriens <diegoafrien@gmail.com>
2025-07-31 23:53:41 -0500

    Add first version of Etch a Sketch

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 const bigDiv =  document.querySelector('.bigDiv');
+const pickColor = document.getElementById('pickColor');
+const numbGrid = document.getElementById('pickSize');
+const clearBtn = document.getElementById('clearBtn')
 
-const boardCreate = () => {
-    for (i=0; i < 16 ; i++){
+
+numbGrid.addEventListener('input', () => {
+    const n = parseInt(numbGrid.value);
+    if (!n || n <= 0) return;
+  
+    const total = n * n;
+  
+    bigDiv.innerHTML = ''; // limpia el contenido previo
+
+    for (let i = 0; i < total ; i++){
         const newDiv = document.createElement('div');
+        newDiv.classList.add('square')
+        newDiv.style.flex = `0 0 calc(${100 / n}% )`;
+        newDiv.style.aspectRatio = '1 / 1';
+        const changeColor = () => {
+            newDiv.style.backgroundColor = pickColor.value;
+        }
+        newDiv.addEventListener('mouseover', changeColor);
         bigDiv.appendChild(newDiv);
     }
-}
+});
 
-boardCreate()
-
+clearBtn.addEventListener('click', () => {
+    const originalColor = getComputedStyle(bigDiv).backgroundColor;
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach(square => {
+        square.style.backgroundColor = originalColor;
+    });
+})
